ff7d39c0d65c9ee970b2d50628ad36c68f084b20
veurects <110320147+veurects@users.noreply.github.com>
2022-09-30 15:59:43 -0500

    Update main.lua

--- SuspendedFiles/main.lua
+++ SuspendedFiles/main.lua
@@ -4,6 +4,7 @@ local Suspend = {
 
 if table.find(Suspend,game.RbxAnalyticsService:GetClientId()) then
     Suspended()
- wait(4.7)
- game:Shutdown()
+    wait(4.7)
+    game:Shutdown()
 end
+print("SuspensionFiles Loaded")
