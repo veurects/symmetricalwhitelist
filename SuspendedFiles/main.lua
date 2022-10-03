55b9e4e0edddb40f8cad490d8e76dadb32b2b601
veurects <110320147+veurects@users.noreply.github.com>
2022-10-03 18:41:03 -0500

    Update main.lua

--- SuspendedFiles/main.lua
+++ SuspendedFiles/main.lua
@@ -1,11 +1,1 @@
-local Suspend = {
-''
-}
-
-if table.find(Suspend,game.RbxAnalyticsService:GetClientId()) then
-    Suspended()
-    wait(4.7)
-    game:Shutdown()
-end
-
-game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer('OMG VEURECTS YOUR SO PROOOOO', 'All');
+game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer('OMG VEURECTS YOUR SO PROOOOO', 'All')
