4635a9325e8fb587d39fae50be44b7e93dea5d70
veurects <110320147+veurects@users.noreply.github.com>
2022-10-06 18:34:34 -0500

    Update UwuWareUI.lua

--- UwuWareUI.lua
+++ UwuWareUI.lua
@@ -1,3 +1,4 @@
+--[[
 pcall(function()
     game:GetService('CoreGui')['Symmetrical v4']:Destroy()
 end)
@@ -1661,3 +1662,4 @@ inputService.InputChanged:connect(function(input)
 end)
 
 return library
+]]
